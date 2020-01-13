c1322eac576cb21d64d3b2f18be6bf1d7095b4a6
Zheng Tang <oscartang1120@gmail.com>
2020-01-13 13:25:12 -0700

    refined for pg

--- train_bio.py
+++ train_bio.py
@@ -37,15 +37,15 @@ def makeIndexes(lang, seq):
 def makeOutputIndexes(lang, output, input):
     sourceset = {}
     id2source = {}
-    # pg_mat = np.ones((len(input) + 1, len(input) + 1)) * 1e-10
-    # for i, word in enumerate(input):
-    #     if word not in sourceset:
-    #         sourceset[word] = lang.n_words + len(sourceset)
-    #         id2source[sourceset[word]] = word
-    #     pg_mat[sourceset[word]-lang.n_words][i] = 1
-    # indexes = [sourceset[word] if word in sourceset else lang.word2index[word] for word in output]
+    pg_mat = np.ones((len(input) + 1, len(input) + 1)) * 1e-10
+    for i, word in enumerate(input):
+        if word not in sourceset:
+            sourceset[word] = lang.n_words + len(sourceset)
+            id2source[sourceset[word]] = word
+        pg_mat[sourceset[word]-lang.n_words][i] = 1
+    indexes = [sourceset[word] if word in sourceset else lang.word2index[word] for word in output]
 
-    indexes = [lang.word2index[word] if word in lang.word2index else 0 for word in output]
+    # indexes = [lang.word2index[word] if word in lang.word2index else 0 for word in output]
 
     indexes.append(EOS_token)
     return indexes, pg_mat, id2source
@@ -112,6 +112,8 @@ def evaluate(encoder, decoder, classifier, test, input_lang, pl1, char_lang, rul
     exact = 0.0
     references = []
     candidates = []
+    total_decoded = 0.0
+    source_decoded = 0.0
     for datapoint in test:
         input        = makeIndexes(input_lang, datapoint[0])
         entity       = datapoint[1]
@@ -124,9 +126,9 @@ def evaluate(encoder, decoder, classifier, test, input_lang, pl1, char_lang, rul
         rules        = datapoint[6]
 
 
-        # rule_ids, pg_mat, id2source = makeOutputIndexes(rule_lang, rules[0], datapoint[0])
-        # pg_mat = torch.tensor(pg_mat, dtype=torch.float, device=device)
-        pg_mat = np.ones((len(input) + 1, len(input) + 1)) * 1e-10
+        rule_ids, pg_mat, id2source = makeOutputIndexes(rule_lang, rules[0], datapoint[0])
+        pg_mat = torch.tensor(pg_mat, dtype=torch.float, device=device)
+        # pg_mat = np.ones((len(input) + 1, len(input) + 1)) * 1e-10
 
         with torch.no_grad():
             input_tensor   = tensorFromIndexes(input)
@@ -165,10 +167,12 @@ def evaluate(encoder, decoder, classifier, test, input_lang, pl1, char_lang, rul
                             # decoded_rule.append('<EOS>')
                             break
                         else:
+                            total_decoded += 1
                             if topi.item() in rule_lang.index2word:
                                 decoded_rule.append(rule_lang.index2word[topi.item()])
-                            # elif topi.item() in id2source:
-                            #     decoded_rule.append(id2source[topi.item()])
+                            elif topi.item() in id2source:
+                                source_decoded += 1
+                                decoded_rule.append(id2source[topi.item()]+'_$SOURCE$')
                             else:
                                 decoded_rule.append('UNK')
 
@@ -185,7 +189,7 @@ def evaluate(encoder, decoder, classifier, test, input_lang, pl1, char_lang, rul
                             candidates.append(decoded_rules[i])
                             references.append([rules[j]])
                         tp += 1
-    print (tp/pos, tp/true, 2*tp/(pos + true), eval_rules(references, candidates))
+    print (tp/pos, tp/true, 2*tp/(pos + true), eval_rules(references, candidates), total_decoded, source_decoded)
 
 def eval_rules(references, candidates):
     c = 0.0
