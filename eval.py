337dea52e2b96cf303a57c87635ef13ff6692cfa
Zheng Tang <oscartang1120@gmail.com>
2020-02-21 16:41:09 -0700

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,4 +1,5 @@
 from train_bio import *
+from sklearn.cluster import AffinityPropagation
 
 
 if __name__ == '__main__':
@@ -23,6 +24,20 @@ if __name__ == '__main__':
     input_lang, pl1, char_lang, rule_lang, raw_train   = prepare_data_from_json(args.jfile, input_lang, pl1, char_lang, rule_lang, raw_train)
     input2_lang, pl2, char_lang2, rule_lang2, raw_test = prepare_data_from_json(args.jfile2)
 
+    deps2id = dict()
+    deps = list()
+    word2id = dict()
+    words = list()
+    for datapoint in raw_train:
+        for i, word in enumerate(datapoint[0]):
+            if i%2 == 0:
+                if word not in deps:
+                    deps.append(word)
+                    deps2id[word] = input_lang.word2index[word]
+            else:
+                if word not in words:
+                    words.append(word)
+                    word2id[word] = input_lang.word2index[word]
 
     epoch = args.epoch
     # for epoch in range(20):
@@ -36,4 +51,19 @@ if __name__ == '__main__':
     decoder.eval()
     classifier.eval()
 
-    evaluate(encoder, decoder, classifier, raw_test, input_lang, pl1, char_lang, rule_lang)
+    wvs = np.zeros((len(words), 100))
+    dvs = np.zeros((len(deps), 100))
+    for i, d in enumerate(deps):
+        d_tensor = torch.tensor([deps2id[d]], dtype=torch.long, device=device).view(-1, 1)
+        dvs[i] = d_tensor.numpy()
+    for i, w in enumerate(words):
+        w_tensor = torch.tensor([word2id[w]], dtype=torch.long, device=device).view(-1, 1)
+        wvs[i] = w_tensor.numpy()
+
+    X = np.concatenate((dvs, wvs), axis=0)
+    print (X)
+    af = AffinityPropagation(preference=-50).fit(X)
+    labels = af.labels_
+    print (labels)
+
+    # evaluate(encoder, decoder, classifier, raw_test, input_lang, pl1, char_lang, rule_lang)
