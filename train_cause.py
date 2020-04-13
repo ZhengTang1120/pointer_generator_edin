3995148579f7db07c9a1c685dda854041c8e19c0
Zheng Tang <oscartang1120@gmail.com>
2020-04-13 09:47:55 -0700

    Update train_cause.py

--- train_cause.py
+++ train_cause.py
@@ -213,8 +213,8 @@ if __name__ == '__main__':
         print (total_loss)
 
         evaluate(encoder, classifier, decoder, raw_test, input_lang, rule_lang)
-        os.mkdir("model_cause_%s/%d"%(args.train, epoch))
-        PATH = "model_cause_%s/%d"%(args.train, epoch)
+        os.mkdir("model_cause_d%s/%d"%(args.train, epoch))
+        PATH = "model_cause_d%s/%d"%(args.train, epoch)
         torch.save(encoder, PATH+"/encoder")
         torch.save(classifier, PATH+"/classifier")
         torch.save(decoder, PATH+"/decoder")
