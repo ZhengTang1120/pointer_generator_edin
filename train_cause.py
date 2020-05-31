11c905e45da25b57234c17d6e67837f83252d338
Zheng Tang <oscartang1120@gmail.com>
2020-05-30 23:34:34 -0700

    Update train_cause.py

--- train_cause.py
+++ train_cause.py
@@ -159,13 +159,12 @@ def get_topi(decoder_output, rule_lang, id2source, lsb, part, prev):
 
     # topv, topi = decoder_output.topk(1)
 
-    # if topi.item() in rule_lang.index2word:
-    #     decoded = rule_lang.index2word[topi.item()]
-    # elif topi.item() in id2source:
-    #     decoded = id2source[topi.item()]
+    if topi.item() in rule_lang.index2word:
+        decoded = rule_lang.index2word[topi.item()]
+    elif topi.item() in id2source:
+        decoded = id2source[topi.item()]
     # else:
     #     decoded = 'UNK'
-        # decoded_rule.append('UNK')
 
     return topi, decoded, lsb, part
 
