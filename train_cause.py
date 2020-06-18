5ca234d1cac2fa3a5341cbb7b525b2229b8e9e9f
Zheng Tang <oscartang1120@gmail.com>
2020-06-18 13:20:58 -0700

    ablation test 2

--- train_cause.py
+++ train_cause.py
@@ -131,22 +131,22 @@ def get_topi(decoder_output, rule_lang, id2source, lsb, part, prev):
     eq_id  = rule_lang.word2index['=']
 
     skip_ids = list(range(rule_lang.n_words+len(id2source), decoder_output.size(1)))
-    # dps      = dp_pattern[:]
-    # words    = w_pattern[:]
-    # for p in id2source:
-    #     if check_dp(id2source[p]):
-    #         dps.append(p)
-    #     else:
-    #         words.append(p)
-    if lsb:
-        skip_ids.append(lsb_id)
-    else:
-        skip_ids.append(rsb_id)
+    dps      = dp_pattern[:]
+    words    = w_pattern[:]
+    for p in id2source:
+        if check_dp(id2source[p]):
+            dps.append(p)
+        else:
+            words.append(p)
+    # if lsb:
+    #     skip_ids.append(lsb_id)
+    # else:
+    #     skip_ids.append(rsb_id)
 
-    # if part == 'word/lemma':
-    #     skip_ids += dps
-    # elif part == 'cause/effect':
-    #     skip_ids += words
+    if part == 'word/lemma':
+        skip_ids += dps
+    elif part == 'cause/effect':
+        skip_ids += words
     topi = top_skipIds(topis, skip_ids)
     if topi.item() == rsb_id:
         lsb = False
