449da7980d5c395e19af8d3d2f9eb1518f86690d
Zheng Tang <oscartang1120@gmail.com>
2020-06-24 23:02:38 -0700

    Update eval_causal.py

--- eval_causal.py
+++ eval_causal.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     # with open('test_x.json'%args.train, 'w') as f:
     #     f.write(json.dumps(raw_test))
 
-    epoch = 2
+    epoch = 4
     PATH = "model_cause_new3/%d"%int(epoch)
     encoder = torch.load(PATH+"/encoder")
     classifier = torch.load(PATH+"/classifier")
