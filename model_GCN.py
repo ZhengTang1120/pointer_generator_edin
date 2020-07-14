a9a54993a4a5b13dd3718927e55367b2c8778a68
Zheng Tang <oscartang1120@gmail.com>
2020-07-14 00:17:49 -0700

    Update model_GCN.py

--- model_GCN.py
+++ model_GCN.py
@@ -25,8 +25,6 @@ class EncoderRNN(nn.Module):
         
         self.rnn = nn.LSTM(embedding_size + 5, hidden_size, bidirectional=True)
 
-        self.gcn = GCNConv(hidden_size, hidden_size)
-
         self.linear  = nn.Linear(hidden_size * 2,   hidden_size)
         self.linear2 = nn.Linear(hidden_size + 610, hidden_size)
 
@@ -49,7 +47,6 @@ class EncoderRNN(nn.Module):
         output, hidden = self.rnn(embedded)
         output  = self.linear(output)
         outputs = output.view(input.size(0), -1)
-        outputs = self.gcn(outputs, edge_index)
         
         cause_vec = outputs[cause_pos[0]:cause_pos[-1]+1]
         effect_vec = outputs[effect_pos[0]:effect_pos[-1]+1]
@@ -76,14 +73,14 @@ class Classifier(nn.Module):
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, i, encoder_outputs, syn_embeddeds, cause, effect, edge_index):
-        # edge_weights = F.softmax(
-        #     torch.mm(
-        #         self.attn(encoder_outputs[i].view( 1,-1)), torch.t(syn_embeddeds)
-        #         )
-        #     , dim=1)
-        # edge_weights = edge_weights.squeeze(0)
-        # outputs = self.gcn(encoder_outputs, edge_index)
-        output = torch.cat((encoder_outputs[i], cause, effect))
+        edge_weights = F.softmax(
+            torch.mm(
+                self.attn(encoder_outputs[i].view( 1,-1)), torch.t(syn_embeddeds)
+                )
+            , dim=1)
+        edge_weights = edge_weights.squeeze(0)
+        outputs = self.gcn(encoder_outputs, edge_index)
+        output = torch.cat((outputs[i], cause, effect))
         output = self.sigmoid(self.out(output))
         return output
 
