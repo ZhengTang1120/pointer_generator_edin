9f826b94a345b02b27c10c9fc7ac7b024396f917
Zheng Tang <oscartang1120@gmail.com>
2020-08-03 16:56:54 -0700

    Update train_new.py

--- train_new.py
+++ train_new.py
@@ -22,9 +22,9 @@ def makeOutputIndexes(lang, output, labels):
             sourceset[label] = lang.n_words + len(sourceset)
             id2source[sourceset[label]] = label
         pg_mat[sourceset[label]-lang.n_words][i] = 1
-    # indexes = [sourceset[token] if token in sourceset else lang.word2index[token] for token in output]
+    indexes = [sourceset[token] if token in sourceset else lang.word2index[token] for token in output]
     # indexes.reverse()
-    indexes = [lang.word2index[word] if word in lang.word2index else 0 for word in output]
+    # indexes = [lang.word2index[word] if word in lang.word2index else 0 for word in output]
     indexes.append(EOS_token)
     return indexes, pg_mat, id2source
 
