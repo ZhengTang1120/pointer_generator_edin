cd256a85e70d531887faf38df51183bce4380bf2
Zheng Tang <oscartang1120@gmail.com>
2020-08-19 01:03:04 -0700

    fix on loss

--- train_GCN_word_only_no_pg_c.py
+++ train_GCN_word_only_no_pg_c.py
@@ -81,11 +81,11 @@ def train(datapoint, encoder, decoder, classifier, encoder_optimizer, decoder_op
         # if len(rule_info)!=0:
         #     torch.nn.utils.clip_grad_norm_(decoder.parameters(), clipping_value)
 
-        encoder_optimizer.step()
-        classifier_optimizer.step()
-        # decoder_optimizer.step()
+            encoder_optimizer.step()
+            classifier_optimizer.step()
+            # decoder_optimizer.step()
 
-        return loss.item()
+            return loss.item()
 
 def eval(encoder, classifier, decoder, raw, input_lang, depen_lang, rule_lang):
     encoder.eval()
